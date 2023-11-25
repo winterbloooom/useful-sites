5d48e04b8e22f8cd1efb0632bd0bf2df4399f534
winterbloooom <winterbloooom@gmail.com>
2023-11-25 21:17:49 +0900

    seperating color palettes

--- script.js
+++ script.js
@@ -15,6 +15,18 @@ function loadPalettes() {
         .then((json) => json.palettes);
 }
 
+function loadUtils() {
+    return fetch("./websites.json")
+        .then((res) => res.json())
+        .then((json) => json.utils);
+}
+
+function loadIcons() {
+    return fetch("./websites.json")
+        .then((res) => res.json())
+        .then((json) => json.icons);
+}
+
 function showPalettes(palettes) {
     const container = document.querySelector("#colors-wrapper");
     palettes.forEach(pal => {
@@ -40,6 +52,54 @@ function showPalettes(palettes) {
     });
 }
 
+function showWebsites(utils, container_id) {
+    const container = document.querySelector(container_id);
+    utils.forEach(util => {
+        line_tr = Object.assign(
+            document.createElement('tr'),
+            { className: "line" }
+        );
+        pick_td = Object.assign(
+            document.createElement('td'),
+            { className: "pick" }
+        );
+        pick_td.innerText += util.pick.frequently ? "⭐" : "";
+        pick_td.innerText += util.pick.good ? "👍" : "";
+        pick_td.innerText += util.pick.web ? "🌏" : "";
+        name_td = Object.assign(
+            document.createElement('td'),
+            { className: "name" }
+        );
+        link_td = Object.assign(
+            document.createElement('a'),
+            { href: util.url },
+            { innerText: util.name },
+            { target: "_blank"}
+        );
+        desc_td = Object.assign(
+            document.createElement('td'),
+            { className: "description" },
+            { innerText: util.desc }
+        );
+        name_td.appendChild(link_td);
+        line_tr.appendChild(pick_td);
+        line_tr.appendChild(name_td);
+        line_tr.appendChild(desc_td);
+        container.appendChild(line_tr);
+    });
+}
+
+loadIcons().then((utils) => {
+    showWebsites(utils, "#icons-table");
+});
+
+loadUtils().then((utils) => {
+    showWebsites(utils, "#websites-table");
+});
+
+
+
 loadPalettes().then((palettes) => {
     showPalettes(palettes);
 });
+
